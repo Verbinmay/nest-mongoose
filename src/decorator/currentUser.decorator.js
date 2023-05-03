bac1d011615a9913b26bb36d1ee5f19a08f5eafc
Mark_Maistrenko <revanmayfall@gmail.com>
2023-05-04 00:03:03 +0400

    fix

--- src/decorator/currentUser.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.CurrentUserId = void 0;
-var common_1 = require("@nestjs/common");
-exports.CurrentUserId = (0, common_1.createParamDecorator)(function (data, context) {
-    var request = context.switchToHttp().getRequest();
-    return request.user;
-});
--- src/user/dto/view-user.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ViewUserDto = void 0;
-var ViewUserDto = /** @class */ (function () {
-    function ViewUserDto() {
-    }
-    return ViewUserDto;
-}());
-exports.ViewUserDto = ViewUserDto;
