19407ab89f6a80889f069da0018693f238b0d776
Mark_Maistrenko <revanmayfall@gmail.com>
2023-09-12 12:32:52 +0000

    Update functionTest.ts
    add functions creating Question input

--- test/functionTest.ts
+++ test/functionTest.ts
@@ -34,24 +34,6 @@ export const info = {
     users: '/sa/users',
   },
 };
-// url: {
-// blogs: '/blogs/',
-// posts: '/posts/',
-// comments: '/comments/',
-// like: '/like-status/',
-// users: '/users/',
-// auth: {
-// login: '/auth/login',
-// logout: '/auth/logout',
-// refreshToken: '/auth/refresh-token',
-// me: '/auth/me',
-// registration: '/auth/registration',
-// registrationConfirmation: '/auth/registration-confirmation',
-// emailResending: '/auth/registration-email-resending',
-//   },
-//   security: '/security/devices/',
-//   testingDelete: '/testing/all-data',
-// },
 
 //BLOG
 export function createBlogInput() {
@@ -62,28 +44,6 @@ export function createBlogInput() {
   };
 }
 
-// export async function testCreateBlogs(
-//   agent: supertest.SuperAgentTest,
-//   number: number,
-// ) {
-//   const blogs: any = [];
-//   for (let i = 0; i < number; i++) {
-//     const createdBlog = {
-//       name: faker.word.noun(6),
-//       description: faker.lorem.sentence(6),
-//       websiteUrl: faker.internet.url(),
-//     };
-
-//     const result = await agent
-//       .post(info.url.blogs)
-//       .set('Authorization', info.headers.authorization)
-//       .send(createdBlog)
-//       .expect(201);
-//     blogs.push(result.body);
-//   }
-
-//   return blogs;
-// }
 
 //POST
 export function createPostInput() {
@@ -93,31 +53,6 @@ export function createPostInput() {
     content: faker.lorem.sentences(5),
   };
 }
-
-// export async function testCreatePosts(
-//   agent: supertest.SuperAgentTest,
-//   number: number,
-//   blog: ViewBlogDto,
-// ) {
-//   const posts: any = [];
-//   for (let i = 0; i < number; i++) {
-//     const createdPost = {
-//       title: faker.word.noun(6),
-//       shortDescription: faker.lorem.sentence(5),
-//       content: faker.lorem.sentences(5),
-//       blogId: blog.id,
-//     };
-
-//     const result = await agent
-//       .post(info.url.posts)
-//       .set('Authorization', info.headers.authorization)
-//       .send(createdPost)
-//       .expect(201);
-//     posts.push(result.body);
-//   }
-
-//   return posts;
-// }
 
 //USERS
 
@@ -129,27 +64,6 @@ export function createUserInput() {
   };
 }
 
-// export async function testCreateUsers(
-//   agent: supertest.SuperAgentTest,
-//   number: number,
-// ) {
-//   const users: any = [];
-//   for (let i = 0; i < number; i++) {
-//     const createdPost = createUserInput();
-
-//     const result = await agent
-//       .post(info.url.users)
-//       .set('Authorization', info.headers.authorization)
-//       .send(createdPost)
-//       .expect(201);
-//     if (result.body) {
-//       users.push(result.body);
-//     }
-//   }
-
-//   return users;
-// }
-
 // //COMMENTS
 
 export function createCommentInput() {
@@ -158,39 +72,17 @@ export function createCommentInput() {
   };
 }
 
-// export async function testCreateComments(a: {
-//   number: number;
-//   post: PostViewModel;
-// }) {
-//   const comments: any = [];
-//   for (let i = 0; i < a.number; i++) {
-//     const inputInfoUser = testInputInfoUser();
-//     const inputInfoComment = testInputInfoComments();
+//QUESTIONS
 
-//     const user = await agent
-//       .post(info.url.users)
-//       .set('Authorization', info.headers.authorization)
-//       .send(inputInfoUser)
-//       .expect(201);
+export function createQuestionInput() {
+  return {
+    body: faker.lorem.sentences(4),
+    correctAnswers: faker.datatype.array(4),
+  };
+}
+export function createPublishedQuestionInput() {
+  return {
+    published: faker.datatype.boolean(),
+  };
+}
 
-//     const login = await agent
-//       .post(info.url.auth.login)
-//       .send({
-//         loginOrEmail: inputInfoUser.email,
-//         password: inputInfoUser.password,
-//       })
-//       .expect(200);
-
-//     const comment = await agent
-//       .post(info.url.posts + a.post.id + info.url.comments)
-//       .send({ ...inputInfoComment, postId: a.post.id })
-//       .set('Authorization', 'Bearer ' + login.body.accessToken)
-//       .expect(201);
-
-//     comments.push(comment.body);
-//   }
-
-//   return comments;
-// }
-
-// //
